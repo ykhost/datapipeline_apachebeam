7b5089de37e11a7dc78410efd3a3dafd13d29a2d
ykhost <andrew.heringer1337@gmail.com>
2022-02-02 17:18:32 +0000

    Processando dados para fazer a uniao dos dois datalake

---
+++ main.py
@@ -0,0 +1,61 @@
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+pipeline_options = PipelineOptions(argv=None)
+pipeline = beam.Pipeline(options=pipeline_options)
+
+colunas_dengue = [
+                'id',
+                'data_iniSE',
+                'casos',
+                'ibge_code',
+                'cidade',
+                'uf',
+                'cep',
+                'latitude',
+                'longitude']
+
+def lista_para_dicionario(elemento, colunas):
+    """
+    Recebe 2 listas
+    Retorna 1 dicionário
+    """
+    return dict(zip(colunas, elemento))
+
+def texto_para_lista(elemento, delimitador='|'):
+    """
+    Recebe um texto e um delimitador
+    Retorna uma lista de elementos pelo delimitador
+    """
+    return elemento.split(delimitador)
+
+def trata_datas(elemento):
+    """
+    Recebe um dicionário e cria um novo campo com ANO-MES
+    Retorna o mesmo dicionário com o novo campo
+    """
+    elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
+    return elemento
+
+def chave_uf(elemento):
+    """
+    Receber um dicionário
+    Retornar uma tupla com o estado(UF) e o elemento (UF, dicionário)
+    """
+    chave = elemento['uf']
+    return (chave, elemento)
+
+dengue = (
+    pipeline
+    | "Leitura do dataset de dengue" >>
+        ReadFromText('casos_dengue.txt', skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
+    | "Criar campo ano_mes" >> beam.Map(trata_datas)
+    | "Criar chave pelo estado" >> beam.Map(chave_uf)
+    | "Agrupar pelo estado" >> beam.GroupByKey()
+    | "Mostrar resultados" >> beam.Map(print)
+)
+
+pipeline.run()
